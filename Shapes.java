f70c194814a74a4444211e5136bb7aa8328abe43
XinBao321 <xinbaoc2@nycstudents.net>
2020-10-16 15:16:30 -0400

    return values

--- Shapes.java
+++ Shapes.java
@@ -1,57 +1,73 @@
 import java.util.Scanner;
 import java.lang.*;
-       public class shapes{
-        public static void main(String[] args) {
- Scanner input = new Scanner(System.in);
- int length1, width1 ,areaTri;
+public class Main{
+public static void main(String[] args) {
 
 
- rectangle(4,5,1);
- circle(1,2);
- System.out.println("What is the base of the triangle ?");
- length1 = input.nextInt();
- System.out.println("What is the height of the triangle ?");
- width1  = input.nextInt();
- areaTri = length1 * width1 /2;
- System.out.println("The area of the triangle is " + areaTri);
- cube(1.0,1.0);
- triangularPrism(1.0,1.0,1.0,1.0,1.0);
- rectangularprism(1.0,1.0,1.0,1.0);
- sphere(1,1.0);
+ System.out.println("\nThe area of this rectangle is: " + rectangle(1,2,3));
+ System.out.println("\nThe area of this circle is: " + circle(1.0,2.0));
+ System.out.println("\nThe area of the triangle is " + triangle(1.0,1.0));
+ System.out.println("\nThe area of triangle using heron's formula is: " + herons(1,2,3));
+ System.out.println("\nThe volume of the cube is: " + cube(1.0,1.0));
+ System.out.println("\nThe volume of the triangleprism is: " + triangularPrism(1.0,1.0,1.0,1.0,1.0));
+ System.out.println("\nThe volume of the rectangularprism is: " + rectangularprism(1.0,1.0,1.0,1.0));
+ System.out.println("\nThe volume of the sphere is: " + sphere(1.0,1.0,1.0));
+
 
 }   //close method
 
-public static void rectangle(int length2,int width2, int areaRectangle){
-  Scanner input = new Scanner(System.in);
-
+public static int rectangle(int length2,int width2, int areaRectangle){
+ Scanner input = new Scanner(System.in);
  System.out.println("Welcome to the shape area calcuator!");
  System.out.println("What is the length of the rectangle?");
  length2 = input.nextInt();
  System.out.println("What is the width of the rectangle?");
  width2 = input.nextInt();
  areaRectangle = length2 * width2;
- System.out.println("The area of the rectangle is " + areaRectangle);
+ return areaRectangle;
+
 
 } //close method for rectangle
-public static void circle(int radius, double area){
+public static double triangle(double base, double height){
+  Scanner input = new Scanner(System.in);
+  System.out.println("What is the base of the triangle ?");
+  base = input.nextDouble();
+  System.out.println("What is the height of the triangle ?");
+  height  = input.nextDouble();
+  double areaTri = base * height /2;
+  return areaTri;
+}//close method for triangle
+
+public static double herons(double length1,double length2, double length3){
+  Scanner input = new Scanner(System.in);
+  System.out.println("What is the length of the first side?");
+  length1 = input.nextDouble();
+  System.out.println("What is the length of the second side?");
+  length2 = input.nextDouble();
+  System.out.println("What is the length of the third side?");
+  length3 = input.nextDouble();
+  double cool =(length1 + length2 + length3)/2;
+  double area = Math.sqrt(cool*(cool - length1)*(cool - length2)*(cool - length3));
+  return area;
+}//end of heron's method
+
+public static double circle(double radius, double area){
  Scanner input = new Scanner(System.in);
  System.out.println("What is the radius of the circle?");
- radius = input.nextInt();
-
+ radius = input.nextDouble();
  area = Math.PI * radius * radius;
-
- System.out.println("The area of the circle " + area);
+ return area;
 } // close method for circle
 
-  public static void cube(double s, double areaCube){
+  public static double cube(double s, double areaCube){
     Scanner input = new Scanner(System.in);
     System.out.println("Enter the side of the cube ");
     s = input.nextDouble();
-    areaCube = s * s * 6;
-    System.out.println("The volume of the cube is " + areaCube);
+    areaCube = Math.pow(s,3);
+    return areaCube;
   } //close method for cube
-  
-  public static void triangularPrism(double length3, double base, double height, double volumeTri, double valueCheck ){
+
+  public static double triangularPrism(double length3, double base, double height, double volumeTri, double valueCheck ){
     Scanner input = new Scanner(System.in);
     System.out.println("What is the length of the triangleprism? ");
     length3 = input.nextDouble();
@@ -60,10 +76,10 @@ public static void circle(int radius, double area){
     System.out.println("What is the height of the triangleprism? ");
     height = input.nextDouble();
     volumeTri = length3 * base * height /2;
-    System.out.println(" The volume of the triangleprism is " + volumeTri);
+    return volumeTri;
   }// close method for triangularprism
 
-  public static void rectangularprism(double length4, double width3, double height2, double volumeRect){
+  public static double rectangularprism(double length4, double width3, double height2, double volumeRect){
     Scanner input = new Scanner(System.in);
     System.out.println("What is the length of the rectangular prism?");
     length4 = input.nextDouble();
@@ -72,15 +88,17 @@ public static void circle(int radius, double area){
     System.out.println("What is the height of the rectangular prism?");
     height2 = input.nextDouble();
     volumeRect = length4 * width3 * height2;
-    System.out.println("The volume of the rectangular prism is " + volumeRect);
+    return volumeRect;
   }  //close method for rectangularprism
 
-    public static void sphere(int radius, double volumeSphere){
+    public static double sphere(double radius, double volumeSphere,double volumeSphere1){
       Scanner input = new Scanner(System.in);
       System.out.println("What is the radius of the sphere?");
-      radius = input.nextInt();
-      volumeSphere = Math.PI * radius * radius * radius * 4/3;
-      System.out.println("The volume of the sphere is " + volumeSphere);
+      radius = input.nextDouble();
+      volumeSphere1 = Math.PI * radius * radius * radius;
+      volumeSphere = volumeSphere1 * 4/3;
+      return volumeSphere;
     } // close method for sphere
 
  }//close class
+      
